4cb8330857a61070752a0ce02332c23f47f58f45
paukasha <odinpozitivchik@mail.ru>
2021-08-24 08:51:43 +1000

    first commit

---
+++ Desktop/proj-vue/proj-vue/src/components/app/SideBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{open: value}">
+    <router-link tag="li"
+                 v-for="link in links"
+                 :key='link.url'
+                 active-class="active"
+                 :to="link.url"
+                 :exact="link.exact">
+      <a href="#" class="waves-effect waves-orange pointer">{{ link.title }}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  data: () => ({
+    links: [
+      {title: 'Счет', url: '/', exact: true},
+      {title: 'История', url: '/history'},
+      {title: 'Планирование', url: '/planning'},
+      {title: 'Новая запись', url: '/record'},
+      {title: 'Категория', url: '/categories'},
+    ]
+  }),
+}
+</script>
